a11abbc84e35fef6e47bf34e2d0041ae0f002553
osama miro <o.miro650@gmail.com>
2017-12-15 02:01:01 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,11 +3,12 @@ def speak_to_grandma
   if "I LOVE YOU GRANDMA!"
   puts "I LOVE YOU TOO PUMPKIN!"
 end
+end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
