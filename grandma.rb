e682bd4b62139d16677b4a34357370918fcc6c87
osama miro <o.miro650@gmail.com>
2017-12-15 02:07:16 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,9 +2,10 @@
 def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA!"
    "I LOVE YOU TOO PUMPKIN!"
-   
+
 elsif string != string.upcase
   "HUH?! SPEAK UP, SONNY!"
+end
   
 end
 # Whatever you say to grandma, she should respond with
